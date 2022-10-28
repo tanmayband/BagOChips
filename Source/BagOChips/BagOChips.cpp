61af674c89e33d25da625ace081213fcfcc134b9
Tanmay Band <tanmayband@gmail.com>
2022-10-29 01:45:07 +0530

    Left out cpp files for custom plugin support

---
+++ Source/BagOChips/BagOChips.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BagOChips.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BagOChips, "BagOChips" );
